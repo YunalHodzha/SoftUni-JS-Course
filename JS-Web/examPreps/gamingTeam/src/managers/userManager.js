9f8eb2adaa0fca7c53f3f4fb56d73b070ca29b84
Yunal Hodzha <yunal.hodzha@abv.bg>
2023-09-26 23:15:51 +0300

    update

--- JS-Web/examPreps/gamingTeam/src/managers/userManager.js
+++ JS-Web/examPreps/gamingTeam/src/managers/userManager.js
@@ -1,10 +1,29 @@
 const bcrypt = require('bcrypt');
+const jwt = require('../lib/jwt');
+const { SECRET } = require('../config/config');
 
 const User = require('../models/User');
 
 exports.login = async (username, password) => {
     const user = await User.findOne({ username });
 
+    if(!user) {
+        throw new Error('Cannot find username ot password');
+    }
+
+    const isValid = await bcrypt.compare(password, user.password);
+    if(!isValid) {
+        throw new Error('Cannot find username ot password');
+    }
+
+    const payload = {
+        _id: user._id,
+        username: user.username
+    };
+
+    const token = await jwt.sign(payload, SECRET, { expiresIn: '2d'});
+
+    return token;
 
 }
 
