63a92d76c07dc8dafe56f817d83566c13e3f118b
Yunal Hodzha <113513080+YunalHodzha@users.noreply.github.com>
2023-09-29 18:49:06 +0300

    update

--- JS-Web/examPreps/scaffold - JS Web exam/src/controllers/itemController.js
+++ JS-Web/examPreps/scaffold - JS Web exam/src/controllers/itemController.js
@@ -1,7 +1,13 @@
 const router = require('express').Router();
+const itemManager = require('../managers/itemManager');
+const { extractErrorMessages } = require('../utils/errorHelpers');
 
 router.get('/create', (req, res) => {
     res.render('items/create');
+});
+
+router.post('/create', (req, res) => {
+    
 });
 
 router.get('/details/', (req, res) => {
