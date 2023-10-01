14c7fafee5ce025fd900522e1928442775a7aecc
Yunal Hodzha <113513080+YunalHodzha@users.noreply.github.com>
2023-10-01 18:33:21 +0300

    update

--- JS-Web/examPreps/gamingTeam/src/controllers/itemController.js
+++ JS-Web/examPreps/gamingTeam/src/controllers/itemController.js
@@ -1,6 +1,7 @@
 const router = require('express').Router();
 const itemManager = require('../managers/itemManager');
 const { extractErrorMessages } = require('../utils/errorHelpers');
+const { getSelectedOption } = require('../utils/viewHelper');
 
 router.get('/catalog', async (req, res) => {
 
@@ -45,21 +46,53 @@ router.get('/details/:itemId', async (req, res) => {
     }
 });
 
-router.get('/:itemId/edit', async (req, res) => {
+router.get('/edit/:itemId', async (req, res) => {
 
     try {
         const item = await itemManager.getItemDetailsById(req.params.itemId).lean();
 
         const isOwner = req.user?._id == item.owner;
 
+        const options = getSelectedOption(item.platform);
+
         if (isOwner) {
-            res.render('items/edit', { item })
+            res.render('items/edit', { item, options });
         } else {
             throw new Error('Not Authorized!')
         }
     } catch (err) {
+        console.log(err)
         res.redirect('/users/login');
     };
+});
+
+router.post('/edit/:itemId', async (req, res) => {
+    const itemId = req.params.itemId;
+    console.log(itemId);
+    try {
+        const item = req.body;
+
+        await itemManager.update(itemId, item);
+
+        res.redirect(`/items/details/${itemId}`);
+    } catch (err) {
+
+        const errorMessage = extractErrorMessages(err);
+        return res.render(`items/edit/${itemId}`, { errorMessage });
+
+    }
+});
+
+router.get('/edit/:itemId/delete', async (req, res) => {
+
+    try {
+        await itemManager.delete(req.params.itemId);
+
+        res.redirect('/items/catalog');
+    } catch (err) {
+        res.render(`/items/${req.params.itemId}/details`, { error: 'Unsuccessful item deletion' });
+    }
+
 });
 
 module.exports = router;
--- JS-Web/examPreps/gamingTeam/src/managers/itemManager.js
+++ JS-Web/examPreps/gamingTeam/src/managers/itemManager.js
@@ -5,3 +5,7 @@ exports.create = (itemData) => Item.create(itemData);
 exports.getAll = () => Item.find();
 
 exports.getItemDetailsById = (itemId) => Item.findById(itemId);
+
+exports.update = (itemId, itemData) => Item.findByIdAndUpdate(itemId, itemData);
+
+exports.delete = (itemID) => Item.findByIdAndDelete(itemID);
